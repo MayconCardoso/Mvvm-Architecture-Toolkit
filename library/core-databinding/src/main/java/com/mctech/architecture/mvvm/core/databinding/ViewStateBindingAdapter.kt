c74511736d365907e414483a2e2b3817a364b623
Maycon Cardoso <maycon.cardoso@dbccompany.com>
2020-04-23 20:42:48 -0300

    - This is a DataBinding extension module with a lot of DataBinding adapters to connect your view model LiveData component state to your view XML file. It helps you to show and hide views easily according to the component state.

--- library/core-databinding/src/main/java/com/mctech/architecture/mvvm/core/databinding/ViewStateBindingAdapter.kt
+++ library/core-databinding/src/main/java/com/mctech/architecture/mvvm/core/databinding/ViewStateBindingAdapter.kt
@@ -7,108 +7,216 @@ import com.mctech.architecture.mvvm.core.ComponentState
 import com.mctech.library.view.ktx.animateShowByState
 
 @BindingAdapter("showOnLoading", requireAll = false)
-fun View.showOnLoading(state: ComponentState<*>?, animate : Boolean = true) {
+fun View.showOnLoading(state: ComponentState<*>?) {
     state?.also {
         showViewByState(
             view = this,
             isToShow = state is ComponentState.Loading,
-            animate = animate
+            animate = false
         )
     }
 }
 
 @BindingAdapter("showOnLoadingFromEmpty", requireAll = false)
-fun View.showOnLoadingFromEmpty(state: ComponentState<*>?, animate : Boolean = true) {
+fun View.showOnLoadingFromEmpty(state: ComponentState<*>?) {
     state?.also {
         showViewByState(
             view = this,
             isToShow = state is ComponentState.Loading.FromEmpty,
-            animate = animate
+            animate = false
         )
     }
 }
 
 @BindingAdapter("showOnLoadingFromData", requireAll = false)
-fun View.showOnLoadingFromData(state: ComponentState<*>?, animate : Boolean = true) {
+fun View.showOnLoadingFromData(state: ComponentState<*>?) {
     state?.also {
         showViewByState(
             view = this,
             isToShow = state is ComponentState.Loading.FromData,
-            animate = animate
+            animate = false
         )
     }
 }
 
 @BindingAdapter("showOnLoadingOrInitializing", requireAll = false)
-fun View.showOnLoadingOrInitializing(state: ComponentState<*>?, animate : Boolean = false) {
+fun View.showOnLoadingOrInitializing(state: ComponentState<*>?) {
     state?.also {
         showViewByState(
             view = this,
             isToShow = state is ComponentState.Loading || state is ComponentState.Initializing,
-            animate = animate
+            animate = false
         )
     }
 }
 
 @BindingAdapter("showOnSuccess", requireAll = false)
-fun View.showOnSuccess(state: ComponentState<*>?, animate : Boolean = true) {
+fun View.showOnSuccess(state: ComponentState<*>?) {
     state?.also {
         showViewByState(
             view = this,
             isToShow = state is ComponentState.Success,
-            animate = animate
+            animate = false
         )
     }
 }
 
 @BindingAdapter("showOnSuccessOrWithData", requireAll = false)
-fun View.showOnSuccessOrWithData(state: ComponentState<*>?, animate : Boolean = true) {
+fun View.showOnSuccessOrWithData(state: ComponentState<*>?) {
     state?.also {
         showViewByState(
             view = this,
             isToShow = state is ComponentState.Success || state is ComponentState.Loading.FromData,
-            animate = animate
+            animate = false
         )
     }
 }
 
 @BindingAdapter("showOnError", requireAll = false)
-fun View.showOnError(state: ComponentState<*>?, animate : Boolean = true) {
+fun View.showOnError(state: ComponentState<*>?) {
     state?.also {
         showViewByState(
             view = this,
             isToShow = state is ComponentState.Error,
-            animate = animate
+            animate = false
         )
     }
 }
 
 @BindingAdapter("hideOnError", requireAll = false)
-fun View.hideOnError(state: ComponentState<*>?, animate : Boolean = true) {
+fun View.hideOnError(state: ComponentState<*>?) {
     state?.also {
         showViewByState(
             view = this,
             isToShow = state !is ComponentState.Error,
-            animate = animate
+            animate = false
         )
     }
 }
 
 @BindingAdapter("showByCondition", requireAll = false)
-fun View.showByCondition(state: Boolean, animate : Boolean = true) {
+fun View.showByCondition(state: Boolean) {
     showViewByState(
         view = this,
         isToShow = state,
-        animate = animate
+        animate = false
     )
 }
 
 @BindingAdapter("showByConditionOrInvisible", requireAll = false)
-fun View.showByConditionOrInvisible(state: Boolean, animate : Boolean = true) {
+fun View.showByConditionOrInvisible(state: Boolean) {
     showViewByState(
         view = this,
         isToShow = state,
-        animate = animate,
+        animate = false,
+        hideState = INVISIBLE
+    )
+}
+
+
+@BindingAdapter("animatedShowOnLoading", requireAll = false)
+fun View.animatedShowOnLoading(state: ComponentState<*>?) {
+    state?.also {
+        showViewByState(
+            view = this,
+            isToShow = state is ComponentState.Loading,
+            animate = true
+        )
+    }
+}
+
+@BindingAdapter("animatedShowOnLoadingFromEmpty", requireAll = false)
+fun View.animatedShowOnLoadingFromEmpty(state: ComponentState<*>?) {
+    state?.also {
+        showViewByState(
+            view = this,
+            isToShow = state is ComponentState.Loading.FromEmpty,
+            animate = true
+        )
+    }
+}
+
+@BindingAdapter("animatedShowOnLoadingFromData", requireAll = false)
+fun View.animatedShowOnLoadingFromData(state: ComponentState<*>?) {
+    state?.also {
+        showViewByState(
+            view = this,
+            isToShow = state is ComponentState.Loading.FromData,
+            animate = true
+        )
+    }
+}
+
+@BindingAdapter("animatedShowOnLoadingOrInitializing", requireAll = false)
+fun View.animatedShowOnLoadingOrInitializing(state: ComponentState<*>?) {
+    state?.also {
+        showViewByState(
+            view = this,
+            isToShow = state is ComponentState.Loading || state is ComponentState.Initializing,
+            animate = true
+        )
+    }
+}
+
+@BindingAdapter("animatedShowOnSuccess", requireAll = false)
+fun View.animatedShowOnSuccess(state: ComponentState<*>?) {
+    state?.also {
+        showViewByState(
+            view = this,
+            isToShow = state is ComponentState.Success,
+            animate = true
+        )
+    }
+}
+
+@BindingAdapter("animatedShowOnSuccessOrWithData", requireAll = false)
+fun View.animatedShowOnSuccessOrWithData(state: ComponentState<*>?) {
+    state?.also {
+        showViewByState(
+            view = this,
+            isToShow = state is ComponentState.Success || state is ComponentState.Loading.FromData,
+            animate = true
+        )
+    }
+}
+
+@BindingAdapter("animatedShowOnError", requireAll = false)
+fun View.animatedShowOnError(state: ComponentState<*>?) {
+    state?.also {
+        showViewByState(
+            view = this,
+            isToShow = state is ComponentState.Error,
+            animate = true
+        )
+    }
+}
+
+@BindingAdapter("animatedHideOnError", requireAll = false)
+fun View.animatedHideOnError(state: ComponentState<*>?) {
+    state?.also {
+        showViewByState(
+            view = this,
+            isToShow = state !is ComponentState.Error,
+            animate = true
+        )
+    }
+}
+
+@BindingAdapter("animatedShowByCondition", requireAll = false)
+fun View.animatedShowByCondition(state: Boolean) {
+    showViewByState(
+        view = this,
+        isToShow = state,
+        animate = true
+    )
+}
+
+@BindingAdapter("animatedShowByConditionOrInvisible", requireAll = false)
+fun View.animatedShowByConditionOrInvisible(state: Boolean) {
+    showViewByState(
+        view = this,
+        isToShow = state,
+        animate = true,
         hideState = INVISIBLE
     )
 }
