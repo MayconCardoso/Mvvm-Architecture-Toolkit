bf0263141d04c334545cd78f296f01deb50888a9
Maycon Cardoso <maycon.cardoso@dbccompany.com>
2020-04-24 18:09:01 -0300

    - This is a networking module to create APIs easily without boilerplate.

---
+++ library/core-networking/src/main/java/com/mctech/library/core/networking/RetrofitBuilder.kt
@@ -0,0 +1,13 @@
+package com.mctech.library.core.networking
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitBuilder {
+    operator fun invoke(apiURL: String, httpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(apiURL)
+        .client(httpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
---
+++ library/core-networking/src/test/java/com/mctech/library/core/networking/SecureRequestHandlerTest.kt
@@ -0,0 +1,23 @@
+package com.mctech.library.core.networking
+
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class SecureRequestHandlerTest{
+
+    @Test
+    fun `should return a known network error`() { runBlocking {
+
+        val result = runCatching {
+            secureRequest<Int>(suspend {
+                throw Throwable()
+            })
+        }.exceptionOrNull()
+
+        Assertions.assertThat(result)
+            .isExactlyInstanceOf(
+                NetworkError.UnknownNetworkingError::class.java
+            )
+    } }
+}
