42423d9885a2d8ba067509e9619d2884dde9398f
mayconcardoso <maycon.santos.cardoso@gmail.com>
2020-05-08 14:19:05 -0300

    - Reflection annotated interaction functions.

--- library/core/src/main/java/com/mctech/architecture/mvvm/x/core/UserInteraction.kt
+++ library/core/src/main/java/com/mctech/architecture/mvvm/x/core/UserInteraction.kt
@@ -1,5 +1,7 @@
 package com.mctech.architecture.mvvm.x.core
 
+import kotlin.reflect.KClass
+
 /**
  * An interaction is some user's 'intention' to do something. For example:
  *  - Load this list for me
@@ -7,3 +9,9 @@ package com.mctech.architecture.mvvm.x.core
  *  - Etc.
  */
 interface UserInteraction
+
+/**
+ * You can annotate your function with this policy to make it be called at runtime.
+ */
+@Target(AnnotationTarget.FUNCTION) @Retention(AnnotationRetention.RUNTIME)
+annotation class OnInteraction(val target : KClass<out UserInteraction>)
--- sample/src/main/java/com/mctech/architecture/mvvm/presentation/ImageViewModel.kt
+++ sample/src/main/java/com/mctech/architecture/mvvm/presentation/ImageViewModel.kt
@@ -9,6 +9,7 @@ import com.mctech.architecture.mvvm.domain.interactions.LoadImageDetailsCase
 import com.mctech.architecture.mvvm.domain.interactions.LoadImageListCase
 import com.mctech.architecture.mvvm.x.core.BaseViewModel
 import com.mctech.architecture.mvvm.x.core.ComponentState
+import com.mctech.architecture.mvvm.x.core.OnInteraction
 import com.mctech.architecture.mvvm.x.core.UserInteraction
 import com.mctech.architecture.mvvm.x.core.ktx.changeToErrorState
 import com.mctech.architecture.mvvm.x.core.ktx.changeToListLoadingState
@@ -26,13 +27,29 @@ class ImageViewModel(
     private val _imageDetailsComponent = MutableLiveData<ComponentState<ImageDetails>>()
     val imageDetailsComponent : LiveData<ComponentState<ImageDetails>> = _imageDetailsComponent
 
-    override suspend fun handleUserInteraction(interaction: UserInteraction) {
-        when(interaction){
-            is ImageInteraction.LoadImages  -> loadImagesInteraction()
-            is ImageInteraction.OpenDetails -> openImageDetailsInteraction(interaction.image)
+    @OnInteraction(ImageInteraction.OpenDetails::class)
+    private suspend fun openImageDetailsInteraction(interaction : ImageInteraction.OpenDetails) {
+        // Set the details component with 'loading' state.
+        _imageDetailsComponent.changeToLoadingState()
+
+        // Open the details screen.
+        sendCommand(ImageCommands.OpenImageDetails)
+
+        // Load image's details.
+        when(val detailsResult = loadImageDetailsCase.execute(interaction.image)){
+            // Set the details component with 'Success' state.
+            is InteractionResult.Success -> {
+                _imageDetailsComponent.changeToSuccessState(detailsResult.result)
+            }
+
+            // Set the details component with 'Error' state.
+            is InteractionResult.Error -> {
+                _imageDetailsComponent.changeToErrorState(detailsResult.error)
+            }
         }
     }
 
+    @OnInteraction(ImageInteraction.LoadImages::class)
     private suspend fun loadImagesInteraction() {
         // Set the list component with 'loading' state.
         _imageListComponent.changeToListLoadingState()
@@ -47,27 +64,6 @@ class ImageViewModel(
             // Set the list component with 'Error' state.
             is InteractionResult.Error -> {
                 _imageListComponent.changeToErrorState(listResult.error)
-            }
-        }
-    }
-
-    private suspend fun openImageDetailsInteraction(image: Image) {
-        // Set the details component with 'loading' state.
-        _imageDetailsComponent.changeToLoadingState()
-
-        // Open the details screen.
-        sendCommand(ImageCommands.OpenImageDetails)
-
-        // Load image's details.
-        when(val detailsResult = loadImageDetailsCase.execute(image)){
-            // Set the details component with 'Success' state.
-            is InteractionResult.Success -> {
-                _imageDetailsComponent.changeToSuccessState(detailsResult.result)
-            }
-
-            // Set the details component with 'Error' state.
-            is InteractionResult.Error -> {
-                _imageDetailsComponent.changeToErrorState(detailsResult.error)
             }
         }
     }
